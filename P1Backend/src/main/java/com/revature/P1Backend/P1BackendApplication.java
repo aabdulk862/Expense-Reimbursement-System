0837a62623a05ac50c3618d0d8342dd6b2867837
Adam Abdulkadir <78934570+aabdulk862@users.noreply.github.com>
2024-12-14 10:27:53 -0500

    More setup

--- P1Backend/src/main/java/com/revature/P1Backend/P1BackendApplication.java
+++ P1Backend/src/main/java/com/revature/P1Backend/P1BackendApplication.java
@@ -2,12 +2,19 @@ package com.revature.P1Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.revature.models") //This tells Spring to look in the models package for DB entities
+@ComponentScan("com.revature") //This tells Spring to look in com.revature for Beans (stereotype annotations)
+@EnableJpaRepositories("com.revature.repository") //This tells Spring to look in the repository package for JPA Repositories
 public class P1BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(P1BackendApplication.class, args);
+		System.out.println("Welcome to our Employee Reimbursement System");
 	}
 
 }
